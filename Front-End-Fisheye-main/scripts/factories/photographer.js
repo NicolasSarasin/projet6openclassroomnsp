43a80f9e894662977d1df8836979b33df856626d
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2022-10-06 14:20:11 +0200

    Add files via upload

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -12,20 +12,18 @@ function photographerFactory(data) {
         const h1 = document.createElement("h1"); //Création d'un titre
         const p = document.createElement("p"); //Création d'un paragraphe
         const p2 = document.createElement("p"); //Création d'un paragraphe
-        const link = document.createElement("a"); //création d'un lien par id
+        //const link = document.createElement("a"); //création d'un lien par id
         h2.textContent = name;
         h1.textContent = city + ", " + country;
         p.textContent = tagline;
         p2.textContent = price + "€/jour";
         p2.style.color = "#929292";
-        link.textContent = id;
-        link.href = photographer.html;
+        //link.href = photographer.html;
         article.appendChild(img); //Image du photographe
         article.appendChild(h2); //Nom du photographe
         article.appendChild(h1); //Pays + ville du photographe
         article.appendChild(p); //Ce que fait le photographe
         article.appendChild(p2); //Prix par jour (exemple 500€/jour)
-        article.appendChild(link);
         return article;
     }
     return { name, picture, getUserCardDOM };
