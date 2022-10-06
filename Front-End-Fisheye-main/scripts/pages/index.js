2d74caf029d38854251d33a0ecc691e480d819ce
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2022-10-06 08:19:56 +0200

    Add files via upload

--- Front-End-Fisheye-main/scripts/pages/index.js
+++ Front-End-Fisheye-main/scripts/pages/index.js
@@ -3,29 +3,10 @@ async function getPhotographers() {
     const data = await response.json();
     const photographers = data.photographers;
     console.log(photographers);
-    /*const photographers = [
-        {
-            name: "Ma data test",
-            id: 1,
-            city: "Paris",
-            country: "France",
-            tagline: "Ceci est ma data test",
-            price: 400,
-            portrait: "account.png",
-        },
-        {
-            name: "Autre data test",
-            id: 2,
-            city: "Londres",
-            country: "UK",
-            tagline: "Ceci est ma data test 2",
-            price: 500,
-            portrait: "account.png",
-        },
-    ];*/
+
     // et bien retourner le tableau photographers seulement une fois
     return {
-        photographers: [...photographers, ...photographers, ...photographers],
+        photographers: [...photographers],
     };
 }
 
