0d4203610099717ba3d8eb6cd66d9496e25dc00b
NicolasSarasin <108725344+NicolasSarasin@users.noreply.github.com>
2022-10-10 08:52:22 +0200

    Add files via upload

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -24,7 +24,7 @@ function photographerFactory(data) {
         article.appendChild(h1); //Pays + ville du photographe
         article.appendChild(p); //Ce que fait le photographe
         article.appendChild(p2); //Prix par jour (exemple 500€/jour)
-        link.appendChild(img);
+        //link.appendChild(img);
         return article;
     }
     return { name, picture, getUserCardDOM };
